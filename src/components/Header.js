fc44401293a19b706919d84873031e1a1c26edc7
Josh <90493234+JoshEddie@users.noreply.github.com>
2023-09-13 15:17:38 -0500

    Nav Added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { SmallLogo } from "./Logo";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <a href="/">
+                <SmallLogo />
+            </a>
+        </header>
+    )
+}
